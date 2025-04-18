fcc4a99e0c830d31f8934026e14ac93615bf3a64
voltingshock <71774880+voltingshock@users.noreply.github.com>
2025-04-18 18:25:24 -0500

    Update createbiogen.java
    fixed leftover examplemod references

--- src/main/java/com/modteamplaceholder/createbiogen/createbiogen.java
+++ src/main/java/com/modteamplaceholder/createbiogen/createbiogen.java
@@ -39,7 +39,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class createbiogen
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "createbiogen";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -60,7 +60,7 @@ public class createbiogen
 
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.examplemod")) //The language key for the title of your CreativeModeTab
+            .title(Component.translatable("itemGroup.createbiogen")) //The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
