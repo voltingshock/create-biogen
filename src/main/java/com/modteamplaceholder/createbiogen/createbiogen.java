ebe60da7a694226e543cec741de0e12d5ff3204d
voltingshock <71774880+voltingshock@users.noreply.github.com>
2025-04-18 16:00:43 -0500

    Mod detail prelim. edits
    mod id and such

--- src/main/java/com/modteamplaceholder/createbiogen/createbiogen.java
+++ src/main/java/com/modteamplaceholder/createbiogen/createbiogen.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.modteamplaceholder.createbiogen;
 
 import org.slf4j.Logger;
 
@@ -35,8 +35,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(createbiogen.MODID)
+public class createbiogen
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -69,7 +69,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public createbiogen(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
